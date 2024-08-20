27255e4fc1f38f8e0a7dcebf56b603a162f0bd86
fluffur <flurffur@gmail.com>
2024-08-20 11:29:11 +0500

    small code corrections

--- scripts/start.js
+++ scripts/start.js
@@ -7,6 +7,7 @@ const fs = require("node:fs").promises;
 const prompt = require("prompt-sync")({sigint: true});
 
 (async () => {
+    console.time('Время выполнения');
 
     const api = new API(process.env.API_ID, process.env.API_HASH, path.resolve(__dirname, '../data/1.json'));
     const auth = new Auth(api);
@@ -14,15 +15,18 @@ const prompt = require("prompt-sync")({sigint: true});
     const app = new App(api, auth);
     await app.checkAuth(process.env.PHONE_NUMBER);
 
-    console.time('Execution Time');
-    const chatName = prompt('Write chat name: ')
-    const users = await app.getUsersFromHistory(chatName, 100);
-    console.timeEnd('Execution Time');
+    const chatName = prompt(
+        'Введите название чата, из которого нужно отправить сообщения пользователям: '
+    )
+    const hundreds = prompt('Введите количество сотен сообщений для получения из истории чата: ')
+    const users = await app.getUsersFromHistory(chatName, parseInt(hundreds));
     await fs.writeFile(path.join(__dirname, '../users.json'), JSON.stringify(users));
 
-    // TODO: Протестировать
-    // const message = prompt('Write message: ');
+    // TODO: Протестировать отправку сообщений
+    // const message = prompt('Введите сообщение, которое вы хотите отправить пользователям: ');
     // await app.sendMessageToUsersFromChat(users, message)
+
+    console.timeEnd('Время выполнения');
 
 })();
 
